bac77328d15a33df8f19646eed1e4b15616232f8
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-06-02 20:30:17 +0200

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,71 +7,29 @@ const Card = ({ movie }) => {
   };
 
   const genreFinder = () => {
-    let genreArray = [];
-    for (let i = 0; i < movie.genre_ids.length; i++) {
-      switch (movie.genre_ids[i]) {
-        case 28:
-          genreArray.push(`Action`);
-          break;
-        case 12:
-          genreArray.push(`Aventure`);
-          break;
-        case 16:
-          genreArray.push(`Animation`);
-          break;
-        case 35:
-          genreArray.push(`Comédie`);
-          break;
-        case 80:
-          genreArray.push(`Policier`);
-          break;
-        case 99:
-          genreArray.push(`Documentaire`);
-          break;
-        case 18:
-          genreArray.push(`Drame`);
-          break;
-        case 10751:
-          genreArray.push(`Famille`);
-          break;
-        case 14:
-          genreArray.push(`Fantasy`);
-          break;
-        case 36:
-          genreArray.push(`Histoire`);
-          break;
-        case 27:
-          genreArray.push(`Horreur`);
-          break;
-        case 10402:
-          genreArray.push(`Musique`);
-          break;
-        case 9648:
-          genreArray.push(`Mystère`);
-          break;
-        case 10749:
-          genreArray.push(`Romance`);
-          break;
-        case 878:
-          genreArray.push(`Science-fiction`);
-          break;
-        case 10770:
-          genreArray.push(`Téléfilm`);
-          break;
-        case 53:
-          genreArray.push(`Thriller`);
-          break;
-        case 10752:
-          genreArray.push(`Guerre`);
-          break;
-        case 37:
-          genreArray.push(`Western`);
-          break;
-        default:
-          break;
-      }
-    }
-    return genreArray.map((genre) => <li key={genre}>{genre}</li>);
+    const genreMapping = {
+      28: "Action",
+      12: "Aventure",
+      16: "Animation",
+      35: "Comédie",
+      80: "Policier",
+      99: "Documentaire",
+      18: "Drame",
+      10751: "Famille",
+      14: "Fantasy",
+      36: "Histoire",
+      27: "Horreur",
+      10402: "Musique",
+      9648: "Mystère",
+      10749: "Romance",
+      878: "Science-fiction",
+      10770: "Téléfilm",
+      53: "Thriller",
+      10752: "Guerre",
+      37: "Western",
+    };
+
+    return movie.genre_ids.map((id) => <li key={id}>{genreMapping[id]}</li>);
   };
 
   const addStorage = () => {
@@ -80,16 +38,16 @@ const Card = ({ movie }) => {
       : [];
 
     if (!storedData.includes(movie.id.toString())) {
-      storedData.push(movie.id);
-      window.localStorage.movies = storedData;
+      storedData.push(movie.id.toString());
+      window.localStorage.movies = storedData.join(",");
     }
   };
 
   const deleteStorage = () => {
     let storedData = window.localStorage.movies.split(",");
-    let newData = storedData.filter((id) => id !== movie.id);
+    let newData = storedData.filter((id) => id !== movie.id.toString());
 
-    window.localStorage.movies = newData;
+    window.localStorage.movies = newData.join(",");
   };
 
   return (
@@ -113,13 +71,13 @@ const Card = ({ movie }) => {
       <ul>
         {movie.genre_ids
           ? genreFinder()
-          : movie.genres.map((genre) => <li key={genre}>{genre.name}</li>)}
+          : movie.genres.map((genre) => <li key={genre.id}>{genre.name}</li>)}
       </ul>
 
-      {movie.overview ? <h3>Synopsis</h3> : ""}
+      {movie.overview && <h3>Synopsis</h3>}
       <p>{movie.overview}</p>
       {movie.genre_ids ? (
-        <div className="btn" onClick={() => addStorage()}>
+        <div className="btn" onClick={addStorage}>
           Ajouter aux coups de coeur
         </div>
       ) : (
